3187971fca3d77b0a1e1cc06662df116495a500f
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-11-29 21:43:01 +0530

    Added Services classes

---
+++ app/services/LibraryManager.java
@@ -0,0 +1,11 @@
+package services;
+
+import dto.Book;
+
+import java.util.List;
+
+public interface LibraryManager {
+    void addBook(String itemName, String authorName, String readerName);
+
+    List<Book> getAllBooks();
+}
