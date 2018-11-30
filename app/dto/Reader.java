6520a9b001839efb1f0da68c64212f7ed31cce08
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-11-30 10:17:02 +0530

    Updated Reader.java with necessary variables and methods

--- app/dto/Reader.java
+++ app/dto/Reader.java
@@ -4,6 +4,15 @@ public class Reader {
 
     private int readerID;
     private String readerName;
+    private String mobileNumber;
+    private String email;
+
+    public Reader(int readerID, String readerName, String mobileNumber, String email) {
+        this.readerID = readerID;
+        this.readerName = readerName;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+    }
 
     public int getReaderID() {
         return readerID;
@@ -19,5 +28,21 @@ public class Reader {
 
     public void setReaderName(String readerName) {
         this.readerName = readerName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
