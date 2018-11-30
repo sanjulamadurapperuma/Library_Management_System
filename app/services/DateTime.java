d28e66fa932736b14eaee00adac3f11fdb26eea8
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-11-30 09:46:04 +0530

    Added DateTime class

---
+++ app/services/DateTime.java
@@ -0,0 +1,130 @@
+package services;
+
+public class DateTime {
+    private int day;
+    private int month;
+    private int year;
+
+    private int hour;
+    private int minute;
+
+    public DateTime(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public DateTime(int hour, int minute){
+        if ( hour >= 1 && hour <= 23){
+            this.hour = hour;
+        } else{
+            this.hour = 0;
+        }
+        if ( minute >= 0 && minute <= 59){
+            this.minute = minute;
+        }
+        else {
+            this.minute = 0;
+        }
+    }
+
+    public DateTime(int day, int month, int year,
+                    int hour, int minute){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+
+        if ( hour >= 1 && hour <= 23){
+            this.hour = hour;
+        } else{
+            this.hour = 0;
+        }
+        if ( minute >= 0 && minute <= 59){
+            this.minute = minute;
+        }
+        else {
+            this.minute = 0;
+        }
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setDay(int day){
+        this.day = day;
+    }
+
+    public void setMonth(int month){
+        this.month = month;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year ){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String getDate(){
+        String formattedDay;
+        if (day < 10){
+            formattedDay = String.format("%02d", day);
+        } else{
+            formattedDay = Integer.toString(getDay());
+        }
+
+        String formattedMonth;
+        if (month < 10) {
+            formattedMonth = String.format("%02d", month);
+        } else{
+            formattedMonth = Integer.toString(getMonth());
+        }
+        return formattedDay + "/" + formattedMonth + "/" + getYear();
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    @Override
+    public String toString() {
+        String time = "";
+        if ( hour < 10 && minute < 10)
+            time = "0" + hour + "0" + minute;
+        else if ( hour < 10 && minute > 10)
+            time = "0" + hour + minute;
+        else if ( hour > 10 && minute < 10)
+            time = hour + "0" + minute;
+        else if ( hour > 10 && minute > 10)
+            time = hour +""+ minute;
+        else if ( hour == 0)
+            time = "0" + hour + minute;
+        else if ( minute == 0)
+            time = hour + "0" + minute;
+        return getDate() + time;
+    }
+}
