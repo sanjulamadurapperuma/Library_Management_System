26b135d5b0ca9dd2d293ab9dda3c5e1fc968f604
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-11-30 09:51:54 +0530

    Completed LibraryItem.java

--- app/dto/LibraryItem.java
+++ app/dto/LibraryItem.java
@@ -1,25 +1,54 @@
 package dto;
 
+import services.DateTime;
+
 public abstract class LibraryItem {
 
-    private int itemID;
-    private String itemName;
+    private int itemISBN;
+    private String itemTitle;
+    private String itemSector;
+    private DateTime publicationDate;
+    private DateTime dateTimeBorrowed;
     private Reader reader;
 
-    public int getItemID() {
-        return itemID;
+    public int getItemISBN() {
+        return itemISBN;
     }
 
-    public void setItemID(int itemID) {
-        this.itemID = itemID;
+    public void setItemISBN(int itemISBN) {
+        this.itemISBN = itemISBN;
     }
 
-    public String getItemName() {
-        return itemName;
+    public String getItemTitle() {
+        return itemTitle;
     }
 
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
+    public void setItemTitle(String itemTitle) {
+        this.itemTitle = itemTitle;
+    }
+
+    public String getItemSector() {
+        return itemSector;
+    }
+
+    public void setItemSector(String itemSector) {
+        this.itemSector = itemSector;
+    }
+
+    public DateTime getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(DateTime publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public DateTime getDateTimeBorrowed() {
+        return dateTimeBorrowed;
+    }
+
+    public void setDateTimeBorrowed(DateTime dateTimeBorrowed) {
+        this.dateTimeBorrowed = dateTimeBorrowed;
     }
 
     public Reader getReader() {
