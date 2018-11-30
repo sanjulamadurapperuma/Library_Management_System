4539dd048d1824ab5b721a30560177ec625836d4
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-11-30 09:56:50 +0530

    Completed Book.java

--- app/dto/Book.java
+++ app/dto/Book.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Book extends LibraryItem {
 
     private List<Author> authors;
+    private String publisher;
+    private String numberOfPages;
 
     public List<Author> getAuthors() {
         return authors;
@@ -12,5 +14,21 @@ public class Book extends LibraryItem {
 
     public void setAuthors(List<Author> authors) {
         this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(String numberOfPages) {
+        this.numberOfPages = numberOfPages;
     }
 }
